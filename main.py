034ff51c46bd7b4b7e48a1e46909824ae6699eb1
Angusvencent <115913884+Angusvencent@users.noreply.github.com>
2023-03-11 22:25:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,46 @@
 import requests, json, re, os
 
 session = requests.session()
+# 机场的地址
+url = os.environ.get('URL')
 # 配置用户名（一般是邮箱）
 email = os.environ.get('EMAIL')
 # 配置用户名对应的密码 和上面的email对应上
 passwd = os.environ.get('PASSWD')
-# server酱
+# server酱 pushdeer
 SCKEY = os.environ.get('SCKEY')
+# 配置cookie
+cookie = os.environ.get('COOKIE')
 
-login_url = 'https://fenda.cloud/auth/login'
-check_url = 'https://fenda.cloud/user/checkin'
-info_url = 'https://fenda.cloud/user/profile'
+# login_url = '{}/auth/login'.format(url)
+check_url = '{}/user/checkin'.format(url)
+
 
 header = {
-        'origin': 'https://fenda.cloud',
+        'cookie': cookie,
+        'origin': url,
         'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
 }
-data = {
-        'email': email,
-        'passwd': passwd
-}
+# data = {
+ #       'email': email,
+ #       'passwd': passwd
+ # }
 try:
-    print('进行登录...')
-    response = json.loads(session.post(url=login_url,headers=header,data=data).text)
-    print(response['msg'])
-    # 获取账号名称
-    info_html = session.get(url=info_url,headers=header).text
-    info = "".join(re.findall('<span class="user-name text-bold-600">(.*?)</span>', info_html, re.S))
-    print(info)
+   # print('进行登录...')
+   # response = json.loads(session.post(url=login_url,headers=header,data=data).text)
+   # print(response['msg'])
     # 进行签到
     result = json.loads(session.post(url=check_url,headers=header).text)
     print(result['msg'])
-    content = info+'\n\n'+result['msg']
+    content = result['msg']
     # 进行推送
     if SCKEY != '':
-        push_url = 'https://api2.pushdeer.com/message/push?pushkey={}&text=fenda机场&desp={}&type=markdown'.format(SCKEY, content)
+        push_url = 'https://api2.pushdeer.com/message/push?pushkey={}&text=ikuu机场签到&desp={}&type=markdown'.format(SCKEY, content)
         requests.post(url=push_url)
         print('推送成功')
 except:
     content = '签到失败'
     print(content)
     if SCKEY != '':
-        push_url = 'https://api2.pushdeer.com/message/push?pushkey={}&text=fenda机场&desp={}&type=markdown'.format(SCKEY, content)
+        push_url = 'https://api2.pushdeer.com/message/push?pushkey={}&text=ikuu机场签到&desp={}&type=markdown'.format(SCKEY, content)
         requests.post(url=push_url)
